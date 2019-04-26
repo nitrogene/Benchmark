ea0ece0ce26d1f6e7f565e3028e8fd15e3aa5aa2
Jean-Pierre GERVASONI <jean.pierre.gervasoni@gmail.com>
2019-04-26 06:59:32 +0200

    Initial commit

---
+++ BenchmarkCore/Program.cs
@@ -0,0 +1,44 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BenchmarkCore
+{
+    [ClrJob(baseline: true), CoreJob, CoreRtJob, MonoJob]
+    [CsvExporter, RankColumn]
+    public class Md5VsSha256
+    {
+        private SHA256 sha256 = SHA256.Create();
+        private MD5 md5 = MD5.Create();
+        private byte[] data;
+
+        [Params(1000, 10000)]
+        public int N;
+
+        [GlobalSetup]
+        public void Setup()
+        {
+            data = new byte[N];
+            new Random(42).NextBytes(data);
+        }
+
+        [Benchmark]
+        public byte[] Sha256() => sha256.ComputeHash(data);
+
+        [Benchmark]
+        public byte[] Md5() => md5.ComputeHash(data);
+    }
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var summary = BenchmarkRunner.Run<Md5VsSha256>();
+        }
+    }
+}
